6db94b70d8a95b02911b359d7f8bc01dbdc3f57b
Mounir khaouadi <mounirr.khaouadi@gmail.com>
2023-09-03 11:52:46 +0200

    website fully responsive

--- myapp/src/Header.js
+++ myapp/src/Header.js
@@ -5,7 +5,7 @@ import logoSvg from './icons_assets/Logo2.svg';
 
 function Header(){
     return(
-        <div className='header-part'>
+        <div className='header_part'>
         <img src={logoSvg} alt='Little lemon logo'/>
         <Nav/>
         </div>
